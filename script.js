b0b1fa3827e90d3840881bae88e0b45952770151
Sooooolhi <castle_heee@naver.com>
2025-01-12 03:38:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById('start-button').addEventListener('click', function() {
-    window.location.href = 'game.html';
-});
+document.getElementById('startButton').onclick = function() {
+    window.location.href = 'detail.html'; // 세부 페이지로 이동
+};
+
+function showPopup(answer) {
+    alert(answer); // 팝업창에 답변 표시
+}
